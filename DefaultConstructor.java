265b4e9886bf08eb0374aef53627971ae2d33075
Selva114 <132544715+Selva114@users.noreply.github.com>
2023-09-23 19:53:15 +0530

    Add files via upload

---
+++ DefaultConstructor.java
@@ -0,0 +1,24 @@
+public class DefaultConstructor
+{
+int a;
+double b;
+char c;
+boolean d;
+public static void main(String args[])
+{
+DefaultConstructor defaultconstructor=new DefaultConstructor();
+System.out.println(defaultconstructor.a);
+System.out.println(defaultconstructor.b);
+System.out.println(defaultconstructor.c);
+System.out.println(defaultconstructor.d);
+}
+}
+
+/*
+Output::
+0
+0.0
+    
+false
+*/
+
